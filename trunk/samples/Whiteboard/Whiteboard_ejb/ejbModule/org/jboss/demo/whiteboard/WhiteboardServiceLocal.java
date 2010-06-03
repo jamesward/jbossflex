bbe6ef863398419643d7557305ed00a051c7e8a6
jlward4th <jlward4th@ee990102-99b0-9f26-2f18-0bcb73a65410>
2010-06-03 02:25:12 +0000

    Initial screen loading now works.
    Polling failover works.

--- trunk/samples/Whiteboard/Whiteboard_ejb/ejbModule/org/jboss/demo/whiteboard/WhiteboardServiceLocal.java
+++ trunk/samples/Whiteboard/Whiteboard_ejb/ejbModule/org/jboss/demo/whiteboard/WhiteboardServiceLocal.java
@@ -13,4 +13,8 @@ public interface WhiteboardServiceLocal
   
   public WhiteboardDTO getWhiteboard(String attendeeId);
   
+  public void drawOnWhiteboard(String whiteboardId, DrawDTO drawDTO);
+  
+  public void eraseWhiteboard(String whiteboardId);
+  
 }
