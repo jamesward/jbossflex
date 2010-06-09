7e9588e54c73af975b45cdb1f905e220927cc68a
robb.greathouse <robb.greathouse@ee990102-99b0-9f26-2f18-0bcb73a65410>
2010-06-09 16:47:25 +0000

    Added Security elements

--- trunk/samples/Whiteboard/Whiteboard_ejb/ejbModule/org/jboss/demo/whiteboard/WhiteboardServiceLocal.java
+++ trunk/samples/Whiteboard/Whiteboard_ejb/ejbModule/org/jboss/demo/whiteboard/WhiteboardServiceLocal.java
@@ -1,20 +1,22 @@
 package org.jboss.demo.whiteboard;
+
 import javax.ejb.Local;
 
 @Local
-public interface WhiteboardServiceLocal
-{
-  
-  public AttendeeDTO createWhiteboard(String whiteboardName, String userDisplayName);
-  
-  public AttendeeDTO connectToWhiteboard(String whiteboardId, String userDisplayName);
-  
-  public void disconnectFromWhiteboard(AttendeeDTO attendee);
-  
-  public WhiteboardDTO getWhiteboard(String attendeeId);
-  
-  public void drawOnWhiteboard(String whiteboardId, DrawDTO drawDTO);
-  
-  public void eraseWhiteboard(String whiteboardId);
-  
+public interface WhiteboardServiceLocal {
+
+	public AttendeeDTO createWhiteboard(String whiteboardName,
+			String userDisplayName);
+
+	public AttendeeDTO connectToWhiteboard(String whiteboardId,
+			String userDisplayName);
+
+	public void disconnectFromWhiteboard(AttendeeDTO attendee);
+
+	public WhiteboardDTO getWhiteboard(String attendeeId);
+
+	public void drawOnWhiteboard(String whiteboardId, DrawDTO drawDTO);
+
+	public void eraseWhiteboard(String whiteboardId);
+
 }
