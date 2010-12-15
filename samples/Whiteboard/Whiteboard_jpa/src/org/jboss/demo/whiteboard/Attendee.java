9632cb69f45515992e6607adb5496d1659b2d22a
jlward4th <jlward4th@ee990102-99b0-9f26-2f18-0bcb73a65410>
2010-12-15 19:32:45 +0000

    Refactored code

--- samples/Whiteboard/Whiteboard_jpa/src/org/jboss/demo/whiteboard/Attendee.java
+++ samples/Whiteboard/Whiteboard_jpa/src/org/jboss/demo/whiteboard/Attendee.java
@@ -1,8 +1,5 @@
 package org.jboss.demo.whiteboard;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.CascadeType;
 import javax.persistence.DiscriminatorColumn;
 import javax.persistence.DiscriminatorType;
